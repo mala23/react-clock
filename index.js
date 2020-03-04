1054b9d969f0ff46ebebf9b96e1f7750da4e868b
mala23 <mail@mala23.me>
2020-03-04 16:30:35 +0100

    converting clock function to class

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function Clock(props) {
-  return (
+class Clock extends React.Component {
+  render() {
+    return (
     <div>
       <h1>Hello, world!</h1>
       <h2>It is {new Date().toLocaleTimeString()}.</h2>
     </div>
-  );
+    );
+  }
 }
 
 function tick() {
