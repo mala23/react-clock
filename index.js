70f61b727a2124ac8bc5760e33e64e1baa354623
mala23 <mail@mala23.me>
2020-03-04 15:13:51 +0100

    hello world jsx

---
+++ index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const name = 'Foo Bar';
+const element = <h1>Hello, {name}</h1>;
+
+ReactDOM.render(
+  element,
+  document.getElementById('root')
+);
+
