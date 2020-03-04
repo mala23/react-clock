337fccb1cd78e5f88496e2a5307f6b25772fd9d8
mala23 <mail@mala23.me>
2020-03-04 15:18:54 +0100

    add a function

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const name = 'Foo Bar';
-const element = <h1>Hello, {name}</h1>;
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
+}
+
+const user = {
+  firstName: 'Foo',
+  lastName: 'Bar'
+};
+
+const element = (
+  <h1>
+    Hello, {formatName(user)}!
+  </h1>
+);
 
 ReactDOM.render(
   element,
