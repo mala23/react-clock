7132f201b91c7198fd7225b3b769e534f16589fe
mala23 <mail@mala23.me>
2020-03-04 16:28:05 +0100

    separate logic from structure

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function tick() {
-  const element = (
+function Clock(props) {
+  return (
     <div>
       <h1>Hello, world!</h1>
-      <h2>It is {new Date().toLocaleTimeString()}</h2>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
     </div>
   );
+}
+
+function tick() {
   ReactDOM.render(
-    element,
+    <Clock date ={new Date()} />,
     document.getElementById('root')
   );
 }
