af166aa6c56dbe0229071189091ca23d9968a693
mala23 <mail@mala23.me>
2020-03-04 16:22:14 +0100

    back to the clock example

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function Welcome(props) {
-  return <h1>Hello, {props.name}</h1>;
+function tick() {
+  const element = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}</h2>
+    </div>
+  );
+  ReactDOM.render(
+    element,
+    document.getElementById('root')
+  );
 }
 
-const element = <Welcome name="Foo" />;
-
-ReactDOM.render(
-  element,
-  document.getElementById('root')
-);
+setInterval(tick, 1000);
