eb52f34a5705ba61e00ee9242949e9c039e2c136
mala23 <mail@mala23.me>
2020-03-04 15:31:15 +0100

    create updating element

--- index.js
+++ index.js
@@ -1,23 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function formatName(user) {
-  return user.firstName + ' ' + user.lastName;
+function tick() {
+  const element = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  ReactDOM.render(element, document.getElementById('root'));
 }
 
-const user = {
-  firstName: 'Foo',
-  lastName: 'Bar'
-};
-
-const element = (
-  <h1>
-    Hello, {formatName(user)}!
-  </h1>
-);
-
-ReactDOM.render(
-  element,
-  document.getElementById('root')
-);
-
+setInterval(tick, 1000);
