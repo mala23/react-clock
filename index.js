abc8d27d80ba5cec23893798085f5ffe6f6c1bd2
mala23 <mail@mala23.me>
2020-03-05 01:17:42 +0100

    move date from props to local state

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
   render() {
     return (
     <div>
       <h1>Hello, world!</h1>
-      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+      <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
     </div>
     );
   }
@@ -14,7 +19,7 @@ class Clock extends React.Component {
 
 function tick() {
   ReactDOM.render(
-    <Clock date ={new Date()} />,
+    <Clock />,
     document.getElementById('root')
   );
 }
